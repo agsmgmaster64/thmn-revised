5a49cceb81591b4af1f6a812635e14626823eb41
Zeta-Patchouli <illiad1461@hotmail.com>
2026-06-18 10:23:20 -0400

    Route 102 and Ichii
    Changed Route 102 Wild Encounters
    Edited events on Route 102
    Added the Ichii ight
    Added the passer-by trainer class

--- src/battle_main.c
+++ src/battle_main.c
@@ -366,6 +366,7 @@ const struct TrainerClass gTrainerClasses[TRAINER_CLASS_COUNT] =
     [TRAINER_CLASS_PIKE_QUEEN] = { _("PIKE QUEEN") },
     [TRAINER_CLASS_PYRAMID_KING] = { _("PYRAMID KING") },
     [TRAINER_CLASS_RS_PROTAG] = { _("{PKMN} TRAINER") },
+	[TRAINER_CLASS_PASSER_BY] = { _("Passer-By"), 10 },
 
     [TRAINER_CLASS_YOUNGSTER_FRLG] =       { _("YOUNGSTER"), 4 },
     [TRAINER_CLASS_BUG_CATCHER_FRLG] =     { _("BUG CATCHER"), 3 },
