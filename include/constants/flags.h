724a707b45a5a1d6a7c0c589299372db9252a209
Zeta-Patchouli <illiad1461@hotmail.com>
2026-07-21 17:19:42 -0400

    Ever Grande Rival and Map Decapitalization
    Changed Southern Pass Location to Sevii Isle 23
    Changed Rivery Pass Location to Weepth Chamber

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1403,8 +1403,8 @@
 #define FLAG_SYS_SAFARI_MODE                        (SYSTEM_FLAGS + 0x2C)
 #define FLAG_SYS_CRUISE_MODE                        (SYSTEM_FLAGS + 0x2D)
 
-#define FLAG_UNUSED_0x88E                           (SYSTEM_FLAGS + 0x2E) // Unused Flag
-#define FLAG_UNUSED_0x88F                           (SYSTEM_FLAGS + 0x2F) // Unused Flag
+#define FLAG_HIDE_EVER_GRANDE_RIVAL                 (SYSTEM_FLAGS + 0x2E) // Formerly Unused Flag
+#define FLAG_DECLINED_RIVAL_BATTLE_EVERGRANDE       (SYSTEM_FLAGS + 0x2F) // Formerly Unused Flag
 
 #define FLAG_SYS_TV_HOME                            (SYSTEM_FLAGS + 0x30)
 #define FLAG_SYS_TV_WATCH                           (SYSTEM_FLAGS + 0x31)
