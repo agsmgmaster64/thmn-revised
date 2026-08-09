07c054344a384772e4f16d218bd99c34e0613906
Zeta-Patchouli <illiad1461@hotmail.com>
2026-08-08 23:27:47 -0400

    Ever Grande and Java Sailor Edits
    Remapped a bit for Ever Grande Isle
    Added a setflag to the aqua grunts to hide the rival blocking Ever Grande
    Scripted Moncoz
    Edited the Border Cave event

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1510,7 +1510,7 @@
 #define FLAG_GRAND_LEAGUE_DEFEATED                  (SYSTEM_FLAGS + 0x85) // Formerly Unused Flag
 #define FLAG_EON_TICKET                             (SYSTEM_FLAGS + 0x86) // Unused Flag
 #define FLAG_OBTAINED_RUBY                          (SYSTEM_FLAGS + 0x87) // Unused Flag
-#define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
+#define FLAG_MONCOZ_KNOWS_YOU                       (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
 #define FLAG_UNUSED_0x8EA                           (SYSTEM_FLAGS + 0x8A) // Unused Flag
 #define FLAG_UNUSED_0x8EB                           (SYSTEM_FLAGS + 0x8B) // Unused Flag
