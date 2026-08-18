a5be22810b203d70ff5707076f916081c3d38c10
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2026-08-18 17:47:04 -0400

    field mugshot setup pt 1

--- src/data/field_mugshots.h
+++ src/data/field_mugshots.h
@@ -3,6 +3,11 @@ static const u32 sFieldMugshotGfx_TestAlt[] = INCGFX_U32("graphics/field_mugshot
 static const u16 sFieldMugshotPal_TestNormal[] = INCGFX_U16("graphics/field_mugshots/test/normal.png", ".gbapal");
 static const u16 sFieldMugshotPal_TestAlt[] = INCGFX_U16("graphics/field_mugshots/test/alt.png", ".gbapal");
 
+static const u32 sFieldMugshotGfx_Renko[] = INCGFX_U16("graphics/field_mugshots/player/renko.png", ".4bpp.smol");
+static const u16 sFieldMugshotPal_Renko[] = INCGFX_U16("graphics/field_mugshots/player/renko.png", ".gbapal");
+static const u32 sFieldMugshotGfx_Maribel[] = INCGFX_U16("graphics/field_mugshots/player/maribel.png", ".4bpp.smol");
+static const u16 sFieldMugshotPal_Maribel[] = INCGFX_U16("graphics/field_mugshots/player/maribel.png", ".gbapal");
+
 struct MugshotGfx
 {
     const u32 *gfx;
@@ -25,6 +30,24 @@ static const struct MugshotGfx sFieldMugshots[MUGSHOT_COUNT][EMOTE_COUNT] =
         {
             .gfx = sFieldMugshotGfx_TestAlt,
             .pal = sFieldMugshotPal_TestAlt,
+        },
+    },
+
+    [MUGSHOT_RENKO] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Renko,
+            .pal = sFieldMugshotPal_Renko,
+        },
+    },
+
+    [MUGSHOT_MARIBEL] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Maribel,
+            .pal = sFieldMugshotPal_Maribel,
         },
     },
 };
